d5b75946975067645b6eb3f6e6cd78482a30850b
LuxionJordan <69374962+LuxionJordan@users.noreply.github.com>
2022-04-19 13:03:50 -0700

    Setup
    2

---
+++ Web/KeyShotLogo/keyshot-configurator-modeldata.js
@@ -0,0 +1,253 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "checked": true,
+                    "icon": "bright%20white.png",
+                    "multimaterials": [
+                        "5+1"
+                    ],
+                    "name": "Bright White"
+                },
+                {
+                    "icon": "white%20paint.png",
+                    "multimaterials": [
+                        "5+2"
+                    ],
+                    "name": "White Paint"
+                },
+                {
+                    "icon": "wireframe%20.png",
+                    "multimaterials": [
+                        "5+3"
+                    ],
+                    "name": "Wireframe "
+                },
+                {
+                    "icon": "hydro%20dip.png",
+                    "multimaterials": [
+                        "5+4"
+                    ],
+                    "name": "Hydro Dip"
+                }
+            ],
+            "name": "White"
+        },
+        {
+            "materials": [
+                {
+                    "icon": "classic.png",
+                    "multimaterials": [
+                        "10+6"
+                    ],
+                    "name": "Classic"
+                },
+                {
+                    "icon": "blue%20leather.png",
+                    "multimaterials": [
+                        "10+7"
+                    ],
+                    "name": "Blue Leather"
+                },
+                {
+                    "checked": true,
+                    "icon": "denim.png",
+                    "multimaterials": [
+                        "10+8"
+                    ],
+                    "name": "Denim"
+                },
+                {
+                    "icon": "hydro%20dip-2.png",
+                    "multimaterials": [
+                        "10+9"
+                    ],
+                    "name": "Hydro Dip"
+                }
+            ],
+            "name": "Blue"
+        },
+        {
+            "materials": [
+                {
+                    "icon": "classic-2.png",
+                    "multimaterials": [
+                        "16+11"
+                    ],
+                    "name": "Classic"
+                },
+                {
+                    "icon": "black%20leather.png",
+                    "multimaterials": [
+                        "16+12"
+                    ],
+                    "name": "Black Leather"
+                },
+                {
+                    "icon": "concrete.png",
+                    "multimaterials": [
+                        "16+13"
+                    ],
+                    "name": "Concrete"
+                },
+                {
+                    "icon": "hydro%20dip-3.png",
+                    "multimaterials": [
+                        "16+14"
+                    ],
+                    "name": "Hydro Dip"
+                },
+                {
+                    "checked": true,
+                    "icon": "ks%20damier.png",
+                    "multimaterials": [
+                        "16+15"
+                    ],
+                    "name": "KS Damier"
+                }
+            ],
+            "name": "Black"
+        },
+        {
+            "materials": [
+                {
+                    "icon": "blue%20leather-2.png",
+                    "multimaterials": [
+                        "24+18"
+                    ],
+                    "name": "Blue Leather"
+                },
+                {
+                    "icon": "wireframe.png",
+                    "multimaterials": [
+                        "24+19"
+                    ],
+                    "name": "Wireframe"
+                },
+                {
+                    "icon": "liquid.png",
+                    "multimaterials": [
+                        "24+20"
+                    ],
+                    "name": "Liquid"
+                },
+                {
+                    "icon": "white%20toon.png",
+                    "multimaterials": [
+                        "24+21"
+                    ],
+                    "name": "White Toon"
+                },
+                {
+                    "icon": "hydro%20dip-4.png",
+                    "multimaterials": [
+                        "24+22"
+                    ],
+                    "name": "Hydro Dip"
+                },
+                {
+                    "checked": true,
+                    "icon": "ks%20damier-2.png",
+                    "multimaterials": [
+                        "24+23"
+                    ],
+                    "name": "KS Damier"
+                }
+            ],
+            "name": "Text "
+        },
+        {
+            "materials": [
+                {
+                    "icon": "blue.png",
+                    "multimaterials": [
+                        "28+25"
+                    ],
+                    "name": "Blue"
+                },
+                {
+                    "checked": true,
+                    "icon": "white.png",
+                    "multimaterials": [
+                        "28+26"
+                    ],
+                    "name": "White"
+                },
+                {
+                    "icon": "black.png",
+                    "multimaterials": [
+                        "28+27"
+                    ],
+                    "name": "Black"
+                }
+            ],
+            "name": "Text Backing"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "icon": "logo.png",
+            "materialgroups": [
+                "White",
+                "Blue",
+                "Black"
+            ],
+            "modelset": 574223244,
+            "name": "Logo",
+            "productoptiongroups": [
+                {
+                    "name": "Letters",
+                    "productoptions": [
+                        {
+                            "checked": true,
+                            "icon": "3d%20letters.png",
+                            "materialgroups": [
+                                "Text ",
+                                "Text Backing"
+                            ],
+                            "modelset": 1057,
+                            "name": "3D Letters"
+                        },
+                        {
+                            "icon": "2d%20letters.png",
+                            "materialgroups": [
+                                "Text "
+                            ],
+                            "modelset": 667436638,
+                            "name": "2D Letters"
+                        }
+                    ]
+                },
+                {
+                    "name": "Component Group"
+                }
+            ]
+        }
+    ],
+    "studios": [
+        {
+            "checked": true,
+            "icon": "iso.png",
+            "id": 570894212,
+            "name": "ISO"
+        },
+        {
+            "icon": "hero.png",
+            "id": 685519783,
+            "name": "Hero"
+        },
+        {
+            "icon": "hero%202.png",
+            "id": 685519857,
+            "name": "Hero 2"
+        },
+        {
+            "icon": "hero%203.png",
+            "id": 570894213,
+            "name": "Hero 3"
+        }
+    ]
+};
